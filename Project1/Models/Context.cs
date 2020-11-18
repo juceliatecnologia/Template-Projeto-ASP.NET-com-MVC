ac188f65a23481ac6e7a7fdc85f35272280fe8c5
Jucelia Ferreira <jucelia.tecnologia@gmail.com>
2020-11-18 10:58:26 -0300

    First commit

---
+++ Project1/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Project1;Integrated Security=True");
+        }
+    }
+}
